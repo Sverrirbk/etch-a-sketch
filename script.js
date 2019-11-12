aa8a92acbdaf17855d28a30f60a7204d417a05bb
Sverrirbk <sverrirbk@gmail.com>
2019-11-12 23:33:17 +0100

    Input button implemented.

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
+grid(64); // initialize grid system
 
-grid(); // initialize grid system
+// get size
+var submitSize = document.getElementById('submitSize');
+submitSize.addEventListener('click', gridSize, false); 
 
+// user input - submitting size of grid
+function gridSize(){
+    var size = document.getElementById("inputSize").value;
+    console.log(size + " is a good size");
+    grid(size); // initialize grid system
+}
 
-
-// creating a grid of 16 div's
-function grid(){
+// creating dynamic grid of divs with unique item
+function grid(size){
     var container = document.getElementById("container");
-    for (var i = 0; i < 16; i++) {
+    for (var i = 0; i < size; i++) {
         var temp = "<div class='item' id='item" + i + "' onmouseover='mouseOver()'>Ready!</div>"        
         container.innerHTML += temp;
         console.log("New div ready to roll!");
     }
 }
 
-// Mouse over changes color
+// mouse over changes color by adding class to generated divs
 function mouseOver(e) {
     this.addEventListener("mouseover", console.log(event.target.id));
     var temp = event.target.id;
     console.log("this is temp " + temp);    
-    document.getElementById(temp).classList.add(".hoverItem");
+    document.getElementById(temp).classList.add("hoverItem");
 }
