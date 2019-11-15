47d462c919b6b092c44c536c9e535d57c2af7bb5
Sverrirbk <sverrirbk@gmail.com>
2019-11-15 22:33:37 +0100

    Finish merge debacle

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // global variables
 var width = getComputedStyle(document.getElementById("container"))
     .getPropertyValue('width');
@@ -8,10 +7,6 @@ var container = document.getElementById("container");
 
 grid(16); // initialize grid system
 
-=======
-grid(64); // initialize grid system
-gridItems = document.querySelectorAll(".item");
->>>>>>> 502702a27851fb5d3cc36ec853d68454a6c4baf4
 
 // user input - get size
 var submitSize = document.getElementById('submitSize');
@@ -19,7 +14,6 @@ submitSize.addEventListener('click', gridSize, false);
 
 // submitting size of grid and initializing grid creation function
 function gridSize(){
-<<<<<<< HEAD
     // start by removing all items from grid
     gridItems = document.querySelectorAll(".item");
     gridItems.forEach(item => container.removeChild(item));
@@ -42,33 +36,17 @@ function grid(inputSize){
     divSize = setItemSize(inputSize) // set item size
     totalDivs = inputSize * inputSize
     for (var i = 0; i < totalDivs; i++) {
-=======
-    gridItems.forEach(item => container.removeChild(item));
-    var size = document.getElementById("inputSize").value;
-    size = size * size
-    grid(size); // initialize grid system
-}
-
-// creating dynamic grid of divs with unique item names and hover functionality
-function grid(size){
-    var container = document.getElementById("container");
-    for (var i = 0; i < size; i++) {
->>>>>>> 502702a27851fb5d3cc36ec853d68454a6c4baf4
         const temp = document.createElement("div");
         temp.className = "item";
         temp.setAttribute("id", "item" + i);
         temp.setAttribute("onmouseover", "mouseOver()");
-<<<<<<< HEAD
         temp.style.height = (divSize + "px");
         temp.style.width = (divSize + "px");
-=======
->>>>>>> 502702a27851fb5d3cc36ec853d68454a6c4baf4
         container.appendChild(temp);      
         console.log("New div ready to roll!");
     }
 }
 
-<<<<<<< HEAD
 function setItemSize(size) {
     width = parseInt(width);
     height = parseInt(height);
@@ -78,8 +56,6 @@ function setItemSize(size) {
     return(divSize);    
 }
 
-=======
->>>>>>> 502702a27851fb5d3cc36ec853d68454a6c4baf4
 // mouse over changes color to black adding css to generated divs
 // possible to add argument that changes color.
 function mouseOver(e) {
